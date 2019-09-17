2ed6e53e86d80b914f404048d422b4dc8720cc45
Jakub Suchanek <jakub@instance-1.c.fastai-sh.internal>
2019-09-17 07:36:29 +0000

    Feat(Vision): Update the vision service

--- Vision/web_app.py
+++ Vision/web_app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+model_name = 'silicon-1.1'
 
 """Extend Python's built in HTTP server to save files
 
@@ -13,26 +14,41 @@ the path can be omitted.
 """
 import os
 from time import sleep
+from fastai.vision import *
+from fastai.metrics import error_rate, accuracy
+from fastai.callbacks import *
+
+import cv2
+import numpy as np
+import os
+import multiprocessing
+from pathlib import Path
+from glob import glob
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 
 try:
     import http.server as server
 except ImportError:
     # Handle Python 2.x
     import SimpleHTTPServer as server
-    
+
 import random
 import string
 
-def randomString(stringLength=10):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
-print ("Random String is ", randomString(5) )
-print ("Random String is ", randomString(10) )
 
 class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
     """Extend SimpleHTTPRequestHandler to handle PUT requests"""
+
+#     def __init__(self, request, client_address, server):
+#         super().__init__(request, client_address, server)
+#         self.model = load_learner('data/models', model_name, test=test)
+        
+    
+    model = load_learner('data/models', model_name)
+
     def do_PUT(self):
         """Save a file following a HTTP PUT request"""
         filename = os.path.basename(self.path)
@@ -40,11 +56,6 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
         # Don't overwrite files
         if os.path.exists(filename):
             print('Overwritten')
-#             self.send_response(409, 'Conflict')
-#             self.end_headers()
-#             reply_body = '"%s" already exists\n' % filename
-#             self.wfile.write(reply_body.encode('utf-8'))
-#             return
 
         file_length = int(self.headers['Content-Length'])
         with open(filename, 'wb') as output_file:
@@ -54,28 +65,37 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
         sleep(3)
         reply_body = f'{randomString(8)}'
         self.wfile.write(reply_body.encode('utf-8'))
-        
+
     def do_POST(self):
         """Save a file following a HTTP POST request"""
         filename = os.path.basename(self.path)
 
-        # Don't overwrite files
-        if os.path.exists(filename):
-            print('Existing name -- no problemo')
-            #self.send_response(409, 'Conflict')
-            #self.end_headers()
-            #reply_body = '"%s" already exists\n' % filename
-            #self.wfile.write(reply_body.encode('utf-8'))
-            #return
-
         file_length = int(self.headers['Content-Length'])
         with open(filename, 'wb') as output_file:
             output_file.write(self.rfile.read(file_length))
+
+        frame = cv2.imread(filename)
+        
+        
+        width = int(frame.shape[1] * 0.5)
+        height = int(frame.shape[0] * 0.5)
+#         dim = (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.5))
+        
+        frame = frame[(frame.shape[0] - height) // 2: (frame.shape[0] + height) // 2,
+                      (frame.shape[1] - width) // 2:  (frame.shape[1] + width) // 2]
+#         frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+#         crop_img = img[y:y+h, x:x+w]
+        cv2.imwrite('cropped.jpg', frame)
+            
+        img = open_image('cropped.jpg')
+        cat, _, prob = self.model.predict(img)
         self.send_response(201, 'Created')
         self.end_headers()
-        sleep(3)
-        reply_body = f'{randomString(8)}\n'
+#         reply_body = f'{cat.strip(['% \n'])}\n'
+        reply_body = str(int(str(cat).strip('% \n'))/100)
+        print(cat, prob)
         self.wfile.write(reply_body.encode('utf-8'))
+
 
 if __name__ == '__main__':
     server.test(HandlerClass=HTTPRequestHandler, port=8080)
